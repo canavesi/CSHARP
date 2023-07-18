0e511bb6ba35d7124a6c473ff754be88f17c7dfe
canavesi <69931119+canavesi@users.noreply.github.com>
2023-07-18 16:14:55 -0300

    Commit ponto 7.

---
+++ AprendendoCSharp/8-Condicionais2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        bool grupo = false;
+
+        // OR ||   and &&
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+
+        // OR ||   and &&
+
+        string TextoAdicional;
+        
+        if (acompanhado == true) 
+        {
+            Console.WriteLine("João está acompanhado");
+        }
+        else 
+        {
+        Console.WriteLine("João nâo está acompanhado");
+        }
+
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
